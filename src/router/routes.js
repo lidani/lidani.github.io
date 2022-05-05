581ff5da21faadce087e70c9ef1726aea773b124
Gustavo Lidani <lidanig0@gmail.com>
2022-05-05 12:20:53 -0300

    route hierarchy

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,28 @@
 const routes = [
   {
     path: "/",
+    name: "main",
     component: () => import("layouts/MainLayout.vue"),
+    children: [
+      {
+        name: "home",
+        path: "",
+        component: () => import("pages/MainPage.vue"),
+      },
+      {
+        name: "apps",
+        path: "apps",
+        component: () => import("layouts/AppsLayout.vue"),
+        children: [
+          {
+            name: "apps-app-information",
+            props: true,
+            path: ":id/:action?",
+            component: () => import("pages/AppInformation.vue"),
+          },
+        ],
+      },
+    ],
   },
 
   // Always leave this as last one,
