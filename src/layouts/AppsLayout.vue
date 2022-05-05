f63859541b4263bcb0cc8eae4c258d023b401a19
Gustavo Lidani <lidanig0@gmail.com>
2022-05-05 12:20:33 -0300

    app list layout

---
+++ src/layouts/AppsLayout.vue
@@ -0,0 +1,43 @@
+<template>
+  <main>
+    <q-page-container class="bg-primary q-pa-lg">
+      <router-view
+        :app="apps[$route.params.id]"
+        v-if="$route.name !== 'apps'"
+      />
+      <div class="flex flex-center page-height" v-else>COMING SOON</div>
+    </q-page-container>
+  </main>
+</template>
+
+<script>
+import { defineAsyncComponent, defineComponent } from "vue";
+
+export default defineComponent({
+  name: "AppsLayout",
+
+  computed: {
+    apps() {
+      return {
+        "tereh-juliana": {
+          title: "Tereh Juliana",
+          actions: {
+            privacy: defineAsyncComponent(() =>
+              import("src/privacy-policy/TerehJuliana.vue")
+            ),
+          },
+          appstore: "",
+          googleplay: "",
+          site: "",
+        },
+        reumathuem: {
+          title: "Reumathuem",
+          appstore: "",
+          googleplay: "",
+          site: "",
+        },
+      };
+    },
+  },
+});
+</script>
