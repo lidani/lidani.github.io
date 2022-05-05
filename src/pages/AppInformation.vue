d17312f4d8a17ece3c2a1cadb516fd95770780c0
Gustavo Lidani <lidanig0@gmail.com>
2022-05-05 12:21:05 -0300

    app information: privacy policy

---
+++ src/pages/AppInformation.vue
@@ -0,0 +1,17 @@
+<template>
+  <template v-if="(app.actions || {}).privacy">
+    <component :is="app.actions.privacy" />
+  </template>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    app: { default: () => ({}) },
+    id: {},
+    action: { required: false },
+  },
+});
+</script>
