2bd6cf336cf0981a65e80f75252d566a5bbccc56
Gustavo Lidani <lidanig0@gmail.com>
2022-05-19 18:13:47 -0300

    remove unecessary script

--- postbuild.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-
-fs.copyFileSync(
-  path.resolve(process.cwd(), "./dist/spa/index.html"),
-  path.resolve(process.cwd(), "./dist/spa/404.html")
-);
