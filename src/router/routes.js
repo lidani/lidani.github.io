af9945255d9fc50f345e0aa7e7c5223926af4c07
Gustavo Lidani <lidanig0@gmail.com>
2022-05-26 18:34:47 -0300

    launch app, better route props

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,3 +1,9 @@
+import apps from "src/constants/apps";
+
+const appsPathProps = (route) => ({
+  app: apps[route.params.app],
+});
+
 const routes = [
   {
     path: "/",
@@ -12,15 +18,23 @@ const routes = [
       {
         name: "apps",
         path: "apps",
+        props: (route) => ({
+          ...appsPathProps(route),
+          action: route.params.action,
+        }),
         component: () => import("layouts/AppsLayout.vue"),
         children: [
           {
             name: "apps-app-information",
-            props: true,
-            path: ":id/:action?",
-            component: () => import("pages/AppInformation.vue"),
+            path: ":app/:action?",
+            component: () => import("pages/apps/AppInformation.vue"),
           },
         ],
+      },
+      {
+        path: "/launch/:app",
+        props: appsPathProps,
+        component: () => import("pages/apps/LaunchApp.vue"),
       },
     ],
   },
