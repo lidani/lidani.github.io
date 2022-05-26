20e185848a08d8fff02a853b6759abc074d616b2
Gustavo Lidani <lidanig0@gmail.com>
2022-05-26 18:34:24 -0300

    click to open app and play stores

---
+++ src/components/stores/AppleStore.vue
@@ -0,0 +1,35 @@
+<template>
+  <a :href="href" @click.stop.prevent="onClick">
+    <q-img
+      class="cursor-pointer"
+      width="136px"
+      height="40px"
+      src="~assets/appstore.png"
+    />
+  </a>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { useQuasar } from "quasar";
+
+export default defineComponent({
+  props: {
+    app: { default: () => ({}) },
+  },
+  setup(props) {
+    const $q = useQuasar();
+
+    const href = `https://itunes.apple.com/us/app/id${
+      props.app.applestore
+    }?platform=${$q.platform.is.ipad ? "ipad" : "iphone"}`;
+
+    return {
+      href,
+      onClick() {
+        return window.open(href, "_blank");
+      },
+    };
+  },
+});
+</script>
---
+++ src/components/stores/PlayStore.vue
@@ -0,0 +1,30 @@
+<template>
+  <a :href="href" @click.stop.prevent="onClick">
+    <q-img
+      class="cursor-pointer"
+      width="136px"
+      height="40px"
+      src="~assets/playstore.png"
+    />
+  </a>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    app: { default: () => ({}) },
+  },
+  setup(props) {
+    const href = `https://play.google.com/store/apps/details?id=${props.app.android}`;
+
+    return {
+      href,
+      onClick() {
+        return window.open(href, "_blank");
+      },
+    };
+  },
+});
+</script>
