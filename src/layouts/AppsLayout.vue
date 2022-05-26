70aa8ed8d30fd1a457d9a86a8dd0575bc06bf741
Gustavo Lidani <lidanig0@gmail.com>
2022-05-26 18:35:22 -0300

    list of app in production

--- src/layouts/AppsLayout.vue
+++ src/layouts/AppsLayout.vue
@@ -1,43 +1,47 @@
 <template>
   <main>
-    <q-page-container class="bg-primary q-pa-lg">
-      <router-view
-        :app="apps[$route.params.id]"
-        v-if="$route.name !== 'apps'"
-      />
-      <div class="flex flex-center page-height" v-else>COMING SOON</div>
+    <q-page-container class="bg-primary q-pa-md">
+      <router-view :app="app" v-if="$route.name !== 'apps'" />
+      <div class="row justify-center" v-else>
+        <div
+          class="column col-xs-12 col-sm-6 col-lg-4 q-pa-md"
+          v-for="(app, i) in apps"
+          :key="i"
+        >
+          <q-card>
+            <AppPreview
+              @image-click="
+                () =>
+                  $router.push({
+                    name: 'apps-app-information',
+                    params: { app: app.id },
+                  })
+              "
+              :app="app"
+            />
+          </q-card>
+        </div>
+      </div>
     </q-page-container>
   </main>
 </template>
 
 <script>
-import { defineAsyncComponent, defineComponent } from "vue";
+import { defineComponent } from "vue";
+
+import apps from "src/constants/apps";
+import AppPreview from "src/components/apps/AppPreview.vue";
 
 export default defineComponent({
   name: "AppsLayout",
-
-  computed: {
-    apps() {
-      return {
-        "tereh-juliana": {
-          title: "Tereh Juliana",
-          actions: {
-            privacy: defineAsyncComponent(() =>
-              import("src/privacy-policy/TerehJuliana.vue")
-            ),
-          },
-          appstore: "",
-          googleplay: "",
-          site: "",
-        },
-        reumathuem: {
-          title: "Reumathuem",
-          appstore: "",
-          googleplay: "",
-          site: "",
-        },
-      };
-    },
+  props: {
+    app: { default: () => ({}) },
   },
+  setup() {
+    return {
+      apps: Object.values(apps).filter((i) => !i.hidden),
+    };
+  },
+  components: { AppPreview },
 });
 </script>
