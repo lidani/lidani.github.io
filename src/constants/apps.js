636ff665c40420a40309e1e6bb0f86f09cb0036f
smallsoft-gustavo <desenvolvimento3@smallsoft.com.br>
2023-05-30 23:04:08 -0300

    feat(apps): added placa-fipe

--- src/constants/apps.js
+++ src/constants/apps.js
@@ -13,6 +13,17 @@ export default {
       ),
     },
   },
+  "placa-fipe": {
+    id: "placa-fipe",
+    title: "Placa Fipe",
+    hasExternalPrivacy: true,
+    ios: "placafipe",
+    applestore: "1663671875",
+    android: "br.com.placafipe",
+    actions: {
+      privacy: "https://placafipe.com.br/politica-de-privacidade/pt-br",
+    },
+  },
   reumathuem: {
     hidden: true,
     id: "reumathuem",
