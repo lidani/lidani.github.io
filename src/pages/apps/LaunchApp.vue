748e4e1578e59f3d3234507c4a14da07172a5091
smallsoft-gustavo <desenvolvimento3@smallsoft.com.br>
2023-05-30 23:05:23 -0300

    refactor: convert launch app to vue3 setup

--- src/pages/apps/LaunchApp.vue
+++ src/pages/apps/LaunchApp.vue
@@ -7,38 +7,47 @@
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import AppPreview from "components/apps/AppPreview.vue";
+import { useQuasar } from "quasar";
+import { useRouter } from "vue-router";
 
 export default defineComponent({
   components: { AppPreview },
   props: {
     app: { default: () => ({}) },
   },
-  async created() {
-    if (!this.app.title) {
-      return this.$router.replace({ name: "home" });
+  setup(props) {
+    const $router = useRouter(),
+      $q = useQuasar();
+
+    if (!props.app.title) {
+      return $router.replace({ name: "home" });
     }
 
-    await new Promise((res) => setTimeout(res, 1000));
+    const launch = () => {
+      if ($q.platform.is.android) {
+        return window.location.replace(
+          `intent://#Intent;scheme=https;package=${props.app.android};end`
+        );
+      }
 
-    this.$q.platform.is.android
-      ? window.location.replace(
-          `intent://#Intent;scheme=https;package=${this.app.android};end`
-        )
-      : this.$q.platform.is.ios
-      ? (() => {
-          window.location.replace(`${this.app.ios}://`);
+      if ($q.platform.is.ios) {
+        window.location.replace(`${props.app.ios}://`);
 
-          window.setTimeout(() => {
-            window.location.replace(
-              `itms-apps://itunes.apple.com/us/app/id${
-                this.app.applestore
-              }?platform=${this.$q.platform.is.ipad ? "ipad" : "iphone"}`
-            );
-          }, 1000);
-        })()
-      : null;
+        setTimeout(() => {
+          window.location.replace(
+            `itms-apps://itunes.apple.com/us/app/id${
+              props.app.applestore
+            }?platform=${$q.platform.is.ipad ? "ipad" : "iphone"}`
+          );
+        }, 1000);
+      }
+    };
+
+    setTimeout(launch, 1000);
+
+    return {};
   },
 });
 </script>
